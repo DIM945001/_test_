10416917bafed10d174a3d00a61b53f9e67c0ad5
DIM945001 <dmitrij1630626@gmail.com>
2021-06-01 22:08:11 +0400

    Initial project version

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Diagnostics;
+using System.Windows.Media.Animation;
+
+namespace WpfApp32
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.wingrid.Background = new SolidColorBrush(Colors.White);
+        }
+
+        private void AuthButton_GotFocus(object sender, RoutedEventArgs e)
+        {
+           
+        }
+
+        private void AuthButton_MouseEnter(object sender, MouseEventArgs e)
+        {
+          //  authButton.Content = "НЕ НАЖИМАТЬ!!!!!!!";
+            
+        }
+
+        private void AuthButton_MouseLeave(object sender, MouseEventArgs e)
+        {
+            authButton.Content = "Авторизация";
+            
+        }
+
+        private void AuthButton_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            //try
+            {
+                MainWindow mw = new MainWindow();
+
+                Window ath = new auth();
+                ath.Show();
+                
+               
+              
+              
+            }
+           // catch
+            {
+                
+             //   return;
+            }
+            
+            
+        }
+
+       
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+           
+            var windowClose = MessageBox.Show("хотите выйти", "окно которое бесит вас", MessageBoxButton.OKCancel, MessageBoxImage.Question, MessageBoxResult.Cancel) == MessageBoxResult.OK;
+            if (windowClose == false)
+            {
+                e.Cancel = true;
+            }
+        }
+
+        private void Title_MouseEnter(object sender, MouseEventArgs e)
+        {
+    
+            
+        }
+        private void Title_MouseLeave(object sender, MouseEventArgs e)
+        {
+           
+        }
+        SolidColorBrush brushL = new SolidColorBrush();
+        SolidColorBrush brush = new SolidColorBrush();
+        ColorAnimation da = new ColorAnimation();
+        ColorAnimation net = new ColorAnimation();
+
+        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            wingrid.Background = brush;
+            da.From = Colors.White;
+            da.To = Colors.Gray;
+            da.Duration = new Duration(TimeSpan.FromSeconds(1));
+            brush.BeginAnimation(SolidColorBrush.ColorProperty, da);
+
+            authButton.Foreground = brushL;
+            title.Foreground = brushL;
+            USLUGI_LINK.Foreground = brushL;
+            net.From = Colors.Black;
+            net.To = Colors.White;
+            net.Duration = new Duration(TimeSpan.FromSeconds(1));
+            brushL.BeginAnimation(SolidColorBrush.ColorProperty, net);
+           
+
+        }
+
+        private void Darkbox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            da.From = Colors.Gray;
+            da.To = Colors.White;
+            da.Duration = new Duration(TimeSpan.FromSeconds(1));
+            brush.BeginAnimation(SolidColorBrush.ColorProperty, da);
+            USLUGI_LINK.Foreground = brushL;
+            authButton.Foreground = brushL;
+            title.Foreground = brushL;
+            net.From = Colors.White;
+            net.To = Colors.Black;
+            net.Duration = new Duration(TimeSpan.FromSeconds(1));
+            brushL.BeginAnimation(SolidColorBrush.ColorProperty, net);
+
+
+        }
+
+        private void Wingrid_GotFocus(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        public void authscs()
+        {
+            
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            uslugi Uslugi = new uslugi();
+            Uslugi.Show();
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+           
+        }
+    }
+}
---
+++ auth.xaml.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Data.SqlClient;
+
+
+namespace WpfApp32
+{
+    /// <summary>
+    /// Логика взаимодействия для auth.xaml
+    /// </summary>
+    public partial class auth : Window
+    {
+        public auth()
+        {
+            InitializeComponent();
+            DataTable dt_user = Select("SELECT * FROM [dbo].[users]"); // получаем данные из таблицы
+            for (int i = 0; i < dt_user.Rows.Count; i++)
+            { // перебираем данные
+                MessageBox.Show(dt_user.Rows[i][0] + "|" + dt_user.Rows[i][1]); // выводим данные
+            }
+        }
+        MainWindow mw = new MainWindow();
+
+        // string login = "login";
+        // string pass = "qwerty";
+
+        public DataTable Select(string selectSQL) // функция подключения к базе данных и обработка запросов
+        {
+            DataTable dataTable = new DataTable("dataBase");                // создаём таблицу в приложении
+                                                                            // подключаемся к базе данных
+            SqlConnection sqlConnection = new SqlConnection("server=CAB212-2;Trusted_Connection=yes;DataBase=AUTH;");
+            sqlConnection.Open();                                           // открываем базу данных
+            SqlCommand sqlCommand = sqlConnection.CreateCommand();          // создаём команду
+            sqlCommand.CommandText = selectSQL;                             // присваиваем команде текст
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(sqlCommand); // создаём обработчик
+            sqlDataAdapter.Fill(dataTable);                                 // возращаем таблицу с результатом
+           
+            return dataTable;
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+                if (textbox1.Text.Length > 0) // проверяем введён ли логин     
+                {
+                    if (textbox2.Password.Length > 0) // проверяем введён ли пароль         
+                    {             // ищем в базе данных пользователя с такими данными         
+                        DataTable dt_user = Select("SELECT * FROM [dbo].[users] WHERE [login] = '" + textbox1.Text + "' AND [password] = '" + textbox2.Password + "'");
+                        if (dt_user.Rows.Count > 0) // если такая запись существует       
+                        {
+                            MessageBox.Show("Пользователь авторизовался"); // говорим, что авторизовался         
+                        }
+                        else MessageBox.Show("Пользователя не найден"); // выводим ошибку  
+                    }
+                    else MessageBox.Show("Введите пароль"); // выводим ошибку    
+                }
+                else MessageBox.Show("Введите логин"); // выводим ошибку 
+            }
+
+
+
+
+
+
+
+
+            //        if (login == textbox1.Text)
+            //        {
+            //            if (pass == textbox2.Password)
+            //            {
+            //                MessageBox.Show("OK");
+            //                DialogResult = true;
+            //                mw.butt.Visibility = Visibility.Hidden;
+            //                mw.lgscs.Visibility = Visibility.Visible;
+            //            }
+            //            else
+            //            {
+            //                if (textbox2.Password == "")
+            //                {
+            //                    MessageBox.Show("где пароль");
+            //                }
+            //                else
+            //                {
+            //                    MessageBox.Show("неверный пароль");
+            //                }
+            //            }
+            //        }
+            //        else
+            //        {
+            //            if (textbox1.Text == "")
+            //            {
+            //                MessageBox.Show("где логин");
+            //            }
+            //            else
+            //            {
+            //                MessageBox.Show("неверный логин");
+            //            }
+
+            //        }
+        }
+
+
+    }
+
